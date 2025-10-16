cf63f2df7e5428ed70fbdf3254a4c5dca716faf2
Oo小刚oO <coolwhg@163.com>
2025-10-16 14:21:50 +0800

    初始化项目

---
+++ .vitepress/config.mjs
@@ -0,0 +1,63 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  lang: 'zh-CN',
+  title: "全栈笔记",
+  description: "软件开发知识分享",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'Node-Red', link: '/docs/node-red/install' }
+    ],
+    
+    sidebar: [
+      {
+        text: 'Node-Red',
+        items: [
+          { text: 'Node-RED 安装', link: '/docs/node-red/install' }
+        ]
+      }
+    ],
+
+    //返回顶部文字修改
+    returnToTopLabel:'返回顶部', 
+    outline: { 
+      level: [2,4], // 显示2-4级标题
+      // level: 'deep', // 显示2-6级标题
+      label: '页面导航' // 文字显示
+    },
+
+    //上次更新时间
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short', // 可选值full、long、medium、short
+        timeStyle: 'medium' // 可选值full、long、medium、short
+      },
+    },
+
+    //本地搜索
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            },
+          },
+        },
+      }
+    }, 
+  }
+})
---
+++ index.md
@@ -0,0 +1,17 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "全栈笔记"
+  text: "软件开发知识分享"
+  tagline: My great project tagline
+  image:
+    src: /home-icon.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: Node-Red
+      link: /docs/node-red/install
+---
+
